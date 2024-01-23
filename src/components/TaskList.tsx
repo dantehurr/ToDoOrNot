79f2f72ab31357b01f679dac1ce305d8bc55b078
dantehurr <75588090+dantehurr@users.noreply.github.com>
2024-01-22 20:34:36 -0500

    Initial commit

---
+++ src/components/TaskList.tsx
@@ -0,0 +1,20 @@
+import Image from "next/image";
+import {} from 'react-icons/fa6';
+
+const TaskList = () => {
+    return (
+        <section id="" className="">
+            <div className="">
+                <div className="bg-gray-300">
+                    <h2 className="">card title</h2>
+                    <p className="">text</p>
+                    <input></input>
+                    <button className="bg-blue-400">button 1</button>
+                    <button className="bg-red-400">button 2</button>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default TaskList;
